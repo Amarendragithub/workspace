35f4a84b5b098c9cd1d432b7603add04da39586b
Amarendragithub <amarendrasahoo787@gmail.com>
2022-10-18 10:51:16 +0530

    test

---
+++ Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+     public static void main(String[]args){
+     int num1=20;
+     int num2=30;
+     int res=num1+num2;
+     System.out.println("result of adding :"+res);
+}
+}
